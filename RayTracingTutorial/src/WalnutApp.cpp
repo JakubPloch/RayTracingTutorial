6af66bd8985a1933e6b32a7fe662f6b22762ab4d
JakubPloch <kubaploch34@gmail.com>
2023-10-07 21:22:30 +0200

    Add Viewport rendering

--- RayTracingTutorial/src/WalnutApp.cpp
+++ RayTracingTutorial/src/WalnutApp.cpp
@@ -3,17 +3,56 @@
 
 #include "Walnut/Image.h"
 
+using namespace Walnut;
+
 class ExampleLayer : public Walnut::Layer
 {
 public:
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+		ImGui::Begin("Settings");
+		if (ImGui::Button("Render"))
+		{
+			Render();
+		}
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("Viewport");
+
+		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
+		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
+
+		if (m_Image)
+		{
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetWidth() });
+		}
+
+		ImGui::End();
+		ImGui::PopStyleVar();
 	}
+
+	void Render() 
+	{
+		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
+		{
+			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+		}
+
+		// loop fill every pixel in the viewport
+		for (uint32_t i = 0; i < m_ViewportHeight * m_ViewportHeight; i++)
+		{
+			m_ImageData[i] = 0xff00ff00;
+		}
+
+		m_Image->SetData(m_ImageData);
+	}
+private:
+	std::shared_ptr<Image> m_Image;
+	uint32_t* m_ImageData = nullptr;
+	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 };
 
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
@@ -24,15 +63,15 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 	Walnut::Application* app = new Walnut::Application(spec);
 	app->PushLayer<ExampleLayer>();
 	app->SetMenubarCallback([app]()
-	{
-		if (ImGui::BeginMenu("File"))
 		{
-			if (ImGui::MenuItem("Exit"))
+			if (ImGui::BeginMenu("File"))
 			{
-				app->Close();
+				if (ImGui::MenuItem("Exit"))
+				{
+					app->Close();
+				}
+				ImGui::EndMenu();
 			}
-			ImGui::EndMenu();
-		}
-	});
+		});
 	return app;
 }
