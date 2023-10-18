73fde30aca70ca4eb4ce30337c21078560dc849a
JakubPloch <kubaploch34@gmail.com>
2023-10-18 08:31:00 +0200

    Render the sphere

--- RayTracingTutorial/Renderer.cpp
+++ RayTracingTutorial/Renderer.cpp
@@ -27,7 +27,7 @@ void Renderer::Render()
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
-
+			coord = coord * 2.0f - 1.0f; // remap to -1 to 1
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
 		}
 	}
@@ -37,9 +37,25 @@ void Renderer::Render()
 
 uint32_t Renderer::PerPixel(glm::vec2 coord)
 {
-	uint8_t r = (uint8_t)(coord.x * 255.0f);
-	uint8_t g = (uint8_t)(coord.y * 255.0f);
+	glm::vec3 rayOrigin(0.0f, 0.0f, -2.0f);
+	glm::vec3 rayDirection(coord.x, coord.y, -1.0f); 
+	float radius = 0.5f;
 
-	return 0xff000000 | (g << 8) | r;
+	// a = ray origin
+	// b = ray direction
+	// r = radius
+	// t = hit distance
+	float a = glm::dot(rayDirection, rayDirection);
+	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
+	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+
+	float discriminant = b * b - 4.0f * a * c;
+
+	if (discriminant >= 0.0f)
+	{
+		return 0xffff00ff;
+	}
+
+	return 0xff000000;
 }
 
