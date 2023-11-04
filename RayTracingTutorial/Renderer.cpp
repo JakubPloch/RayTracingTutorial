7481ed58d95c6662f2d00c757f1dd90faef61f80
JakubPloch <kubaploch34@gmail.com>
2023-11-04 22:38:25 +0100

    Use coords as colors

--- RayTracingTutorial/Renderer.cpp
+++ RayTracingTutorial/Renderer.cpp
@@ -1,6 +1,20 @@
 #include "Walnut/Random.h"
 #include "Renderer.h"
 
+namespace Helpers 
+{
+	static uint32_t ConvertToABGR(const glm::vec4& color) 
+	{
+		uint8_t r = (uint8_t)(color.r * 255.0f);
+		uint8_t g = (uint8_t)(color.g * 255.0f);
+		uint8_t b = (uint8_t)(color.b * 255.0f);
+		uint8_t a = (uint8_t)(color.a * 255.0f);
+
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+		return result;
+	}
+}
+
 void Renderer::OnResize(uint32_t width, uint32_t height)
 {
 	if (m_FinalImage)
@@ -28,17 +42,20 @@ void Renderer::Render()
 		{
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
 			coord = coord * 2.0f - 1.0f; // remap to -1 to 1
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Helpers::ConvertToABGR(color);
 		}
 	}
 
 	m_FinalImage->SetData(m_ImageData);
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord)
+glm::vec4 Renderer::PerPixel(glm::vec2 coord)
 {
-	glm::vec3 rayOrigin(0.0f, 0.0f, -2.0f);
-	glm::vec3 rayDirection(coord.x, coord.y, -1.0f); 
+	glm::vec3 rayOrigin(0.0f, 0.0f, 2.0f);
+	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
 	float radius = 0.5f;
 
 	// a = ray origin
@@ -51,11 +68,21 @@ uint32_t Renderer::PerPixel(glm::vec2 coord)
 
 	float discriminant = b * b - 4.0f * a * c;
 
-	if (discriminant >= 0.0f)
+	if (discriminant <= 0.0f)
 	{
-		return 0xffff00ff;
+		return glm::vec4(0, 0, 0, 1);
 	}
 
-	return 0xff000000;
+	float furtherHitDistance = (-b + glm::sqrt(discriminant)) / (2.0f * a);
+	float closerHitDistance = (-b - glm::sqrt(discriminant)) / (2.0f * a);
+
+	glm::vec3 hitPoint = rayOrigin + rayDirection * closerHitDistance;
+
+	glm::vec3 sphereColor(1, 0, 1);
+	sphereColor = hitPoint;
+	return glm::vec4 (sphereColor, 1.0f);
+
 }
+
+
 
