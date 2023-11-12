6cf9d4d41bbe571b980326833a62d24ddeff262f
JakubPloch <kubaploch34@gmail.com>
2023-11-12 21:15:06 +0100

    Add interactive camera

--- RayTracingTutorial/Renderer.cpp
+++ RayTracingTutorial/Renderer.cpp
@@ -1,9 +1,9 @@
 #include "Walnut/Random.h"
 #include "Renderer.h"
 
-namespace Helpers 
+namespace Helpers
 {
-	static uint32_t ConvertToABGR(const glm::vec4& color) 
+	static uint32_t ConvertToABGR(const glm::vec4& color)
 	{
 		uint8_t r = (uint8_t)(color.r * 255.0f);
 		uint8_t g = (uint8_t)(color.g * 255.0f);
@@ -34,16 +34,18 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 	m_ImageData = new uint32_t[width * height];
 }
 
-void Renderer::Render(glm::vec3 objectColor, glm::vec3 lightSourceCoords)
+void Renderer::Render(const Camera& camera, glm::vec3 objectColor, glm::vec3 lightSourceCoords)
 {
+	Ray ray;
+	ray.Origin = camera.GetPosition();
+
 	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++)
 	{
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
 		{
-			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
-			coord = coord * 2.0f - 1.0f; // remap to -1 to 1
+			ray.Direction = camera.GetRayDirections()[x + y * m_FinalImage->GetWidth()];
 
-			glm::vec4 color = PerPixel(coord, objectColor, lightSourceCoords);
+			glm::vec4 color = TraceRay(ray, objectColor, lightSourceCoords);
 			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = Helpers::ConvertToABGR(color);
 		}
@@ -52,19 +54,17 @@ void Renderer::Render(glm::vec3 objectColor, glm::vec3 lightSourceCoords)
 	m_FinalImage->SetData(m_ImageData);
 }
 
-glm::vec4 Renderer::PerPixel(glm::vec2 coord, glm::vec3 objectColor, glm::vec3 lightSourceCoords)
+glm::vec4 Renderer::TraceRay(const Ray& ray, glm::vec3 objectColor, glm::vec3 lightSourceCoords)
 {
-	glm::vec3 rayOrigin(0.0f, 0.0f, 1.0f);
-	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
 	float radius = 0.5f;
 
 	// a = ray origin
 	// b = ray direction
 	// r = radius
 	// t = hit distance
-	float a = glm::dot(rayDirection, rayDirection);
-	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
-	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
+	float a = glm::dot(ray.Direction, ray.Direction);
+	float b = 2.0f * glm::dot(ray.Origin, ray.Direction);
+	float c = glm::dot(ray.Origin, ray.Origin) - radius * radius;
 
 	float discriminant = b * b - 4.0f * a * c;
 
@@ -76,7 +76,7 @@ glm::vec4 Renderer::PerPixel(glm::vec2 coord, glm::vec3 objectColor, glm::vec3 l
 	float furtherHitDistance = (-b + glm::sqrt(discriminant)) / (2.0f * a);
 	float closerHitDistance = (-b - glm::sqrt(discriminant)) / (2.0f * a);
 
-	glm::vec3 hitPoint = rayOrigin + rayDirection * closerHitDistance;
+	glm::vec3 hitPoint = ray.Origin + ray.Direction * closerHitDistance;
 	glm::vec3 normal = glm::normalize(hitPoint);
 
 	glm::vec3 lightDirection = glm::normalize(lightSourceCoords);
@@ -85,11 +85,6 @@ glm::vec4 Renderer::PerPixel(glm::vec2 coord, glm::vec3 objectColor, glm::vec3 l
 
 	glm::vec3 sphereColor(objectColor);
 	sphereColor *= d; // normal * 0.5f + 0.5f;
-	return glm::vec4 (sphereColor, 1.0f);
+	return glm::vec4(sphereColor, 1.0f);
 
 }
-
-// homework: change color and light direction using imgui
-
-
-
--- RayTracingTutorial/Renderer.h
+++ RayTracingTutorial/Renderer.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "Walnut/Image.h"
-
 #include <memory>
+#include "Camera.h"
+#include "Ray.h"
 
 class Renderer 
 {
@@ -10,11 +11,11 @@ public:
 	Renderer() = default;
 
 	void OnResize(uint32_t width, uint32_t height);
-	void Render(glm::vec3 objectColor, glm::vec3 lightSourceCoords);
+	void Render(const Camera& camera, glm::vec3 objectColor, glm::vec3 lightSourceCoords);
 
 	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; };
 private:
-	glm::vec4 PerPixel(glm::vec2 coord, glm::vec3 objectColor, glm::vec3 lightSourceCoords);
+	glm::vec4 TraceRay(const Ray& ray, glm::vec3 objectColor, glm::vec3 lightSourceCoords);
 
 	std::shared_ptr<Walnut::Image> m_FinalImage;
 	uint32_t* m_ImageData = nullptr;
