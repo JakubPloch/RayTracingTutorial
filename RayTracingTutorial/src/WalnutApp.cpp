628259d4fd130aa96d5286bbc707742aafc3692d
JakubPloch <kubaploch34@gmail.com>
2023-12-05 21:11:19 +0100

    Add multithreading

--- RayTracingTutorial/src/WalnutApp.cpp
+++ RayTracingTutorial/src/WalnutApp.cpp
@@ -22,11 +22,11 @@ public:
 
 		Material& pinkSphere = m_Scene.Materials.emplace_back();
 		pinkSphere.Albedo = { 1.0f, 0.0f, 1.0f };
-		pinkSphere.Roughness = 0.0f;
+		pinkSphere.Roughness = 0.3f;
 
 		Material& blueSphere = m_Scene.Materials.emplace_back();
 		blueSphere.Albedo = { 0.2f, 0.3f, 1.0f };
-		blueSphere.Roughness = 0.1f;
+		blueSphere.Roughness = 0.9f;
 
 		{
 			Sphere sphere;
